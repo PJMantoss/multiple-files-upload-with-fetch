b9503818ed65de57d149f9cfcbd65d53c15e2b34
pjmantoss <joelptoss@gmail.com>
2020-11-07 16:46:18 +0100

    update script

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ uploadForm.addEventListener('submit', function(e){
     uploadFile(this);
 });
 
-const uploadFile = () => {}
+const uploadFile = async data => {
+    const formData = new FormData();
+
+    const files = data.querySelector('input[type="file"]').files;
+
+    for (let i=0; i<files.length; i++){
+        form.append(`fileInput_${i}`, files[i]);
+    }
+
+    const options = {
+        method: "POST",
+        body: formData
+    }
+}
